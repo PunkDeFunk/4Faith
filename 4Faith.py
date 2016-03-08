09ee2663b125171d105a4b0915b649ae451f1f73
PunkDeFunk <ajordan2564@gmail.com>
2016-03-08 00:27:45 -0800

    Added files via upload

---
+++ 4Faith.py
@@ -0,0 +1,93 @@
+# A little love program to/for Faith Rasmussen
+import time
+print ('Hello, are you Faith Rasmussen? [y/n]')
+while True:
+    if_faith = input()
+    if str(if_faith) == 'y':
+        print ('Really? Well then lets just be sure. According to Facebook, whats our anniversary? [mm/dd]')
+        break
+    if str(if_faith) == 'n':
+        print('Im sorry to hear that. Im sure you are a nice person. Unfortunatly this program is not for you.')
+        time.sleep(1.0)
+        print ('....')
+        time.sleep(1.0)
+        print('How did you get this file anyways?')
+        input()
+        print ('You know what?')
+        time.sleep(0.5)
+        print ('I dont care')
+        time.sleep(0.5)
+        print ('Initiating Infinity loop of Hearts to crash your computer....')
+        time.sleep(3.0)
+        n = 1
+        while n < 8000:
+            print ('<3')
+            n = (n + 1)
+        time.sleep(4.0)
+        print('You are still here?')
+        time.sleep(1.0)
+        print('You must be Faith.....')
+        time.sleep(0.5)
+        print ('Ok, what is our anniversary? [mm/dd]')
+        break
+    else:
+        print('try using either "y" or "n"')
+        time.sleep(0.5)
+        print('So are you my Babesauce? [y/n]')
+while True:
+    annav = input()
+    if str(annav) == ('08/20'):
+        print ('<3 BABE! I love you very much! How is your day going?')
+        break
+    else:
+        print('Im sorry to hear that. Im sure you are a nice person. Unfortunatly this program is not for you.')
+        time.sleep(1.0)
+        print ('....')
+        time.sleep(1.0)
+        print('How did you get this file anyways?')
+        input()
+        print ('You know what?')
+        time.sleep(0.5)
+        print ('I dont care')
+        time.sleep(0.5)
+        print ('Initiating Infinity loop of Hearts to crash your computer....')
+        time.sleep(3.0)
+        n = 1
+        while n < 4000:
+            print ('<3')
+            n = (n + 1)
+        time.sleep(4.0)
+        print('You are still here?')
+        time.sleep(1.0)
+        print('You must be Faith.....')
+        time.sleep(0.5)
+        print ('Ok, what is our anniversary? [mm/dd]')
+time.sleep(0.5)
+print('---------------')
+print("Try: Good , Bad , Stressful , Lazy")
+while True:
+    day = input()
+    if str(day) == 'Good':
+        print('Im glad to hear that! Everyday is a good day with you Babe! :)')
+        break
+    if str(day) == 'Bad':
+        print('Im sorry to hear that Babe. Sometimes the going gets tuff, and thats when the Faith gets going.')
+        time.sleep(1.0)
+        print('Just remember that alot of people love you to pieces. Including me! <3')
+        time.sleep(1.0)
+        print ('You are the smartest, coolest, sexiest, person I know! You will figure out how make it better')
+        time.sleep(1.0)
+        print('I will always love you')
+        break
+    elif str(day) == 'Stressful':
+        print ('You must be working on great things')
+        break
+    elif str(day) == 'Lazy':
+        print ('Im sure you earrned it. Netflix and chill? :)')
+        break
+    else:
+        print('Try: "Good" "Bad" "Stressful" or "Lazy"')
+
+              
+        
+
